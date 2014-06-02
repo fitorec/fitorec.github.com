f69c215e5a812a32f7ac95ec3617a9f004e651af
fitorec <chanerec@gmail.com>
2014-06-02 10:56:45 -0500

    Agregando Base de datos

--- ieso/bases_de_datos_II/parcial-2/BD.java
+++ ieso/bases_de_datos_II/parcial-2/BD.java
@@ -1,31 +1,128 @@
 /*
- * @Author: fitorec
-
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 package aplicacion_bd2.core;
 
-import java.sql.ResultSet;
-import org.junit.After;
-import org.junit.Test;
-//import static org.junit.Assert.*;
+import java.sql.*;
 
 /**
  *
  * @author fitorec
  */
-public class BDTest {
-    @After
-    public void tearDown() {
-        BD.finalizar();
+public class BD {
+    private static String login;
+    private static final String loginDefault = "root";
+    private static String password = null;
+    private static final String passwordDefault = "";
+    private static String baseNombre   = null;
+    private static final String baseBDDefault = "clase_bd_2";
+    private static String host     = null;
+    private static final String hostDefault = "localhost";
+    private static Connection conexion;
+
+    public static String config(String campo) {
+        if (campo.equalsIgnoreCase("login")) {
+            if(BD.login == null) {
+                return BD.loginDefault;
+            }
+            return BD.login;
+        }
+        if (campo.equalsIgnoreCase("password")) {
+            if(BD.password == null) {
+                return BD.passwordDefault;
+            }
+            return BD.password;
+        }
+        if (campo.equalsIgnoreCase("baseBD")) {
+            if(BD.baseNombre == null) {
+                return BD.baseBDDefault;
+            }
+            return BD.baseNombre;
+        }
+        if (campo.equalsIgnoreCase("host")) {
+            if(BD.host == null) {
+                return BD.hostDefault;
+            }
+            return BD.host;
+        }
+        return campo;
+    }
+
+    public static String config(String field, String valor) {
+        if (field.equals("login") && BD.login == null) {
+            BD.login = valor;
+            return valor;
+        }
+        if (field.equals("password") && BD.password == null) {
+            BD.password = valor;
+            return BD.password;
+        }
+        if (field.equalsIgnoreCase("baseBD") && BD.baseNombre == null) {
+            BD.baseNombre = valor;
+            return BD.baseNombre;
+        }
+        if (field.equalsIgnoreCase("host") && BD.host == null) {
+            BD.host = valor;
+            return BD.host;
+        }
+        System.out.println("Campo incorrecto de configuración : " + field);
+        return field;
     }
 
     /**
-     * Probando el metodo consulta.
+     * Constructor
+     * @return BD.conexion;
      */
-    @Test
-    public void consultaTest() {
-        ResultSet result = BD.consulta("SELECT * FROM alumnos WHERE 1");
+    public static Connection conexion() {
+        if(BD.conexion != null) {
+           try {
+            String url = "jdbc:mysql://" +
+                BD.config("host") +
+                "/" + BD.config("baseBD");
+            Class.forName("org.gjt.mm.mysql.Driver");
+            BD.conexion = DriverManager.getConnection(
+                url,
+                BD.config("password"),
+                BD.config("contraseña")
+           );
+            } catch(SQLException ex) {
+                ex.getStackTrace();
+            }
+            catch(ClassNotFoundException ex) {
+                System.out.println(ex);
+            }
+        }
+        return BD.conexion;
     }
 
+    public static ResultSet consulta(String strQuery) {
+        Connection c = BD.conexion();
+        ResultSet result = null;
+        if(c != null) {
+            try {
+                Statement stmt = c.createStatement();
+                result = stmt.executeQuery( strQuery );
+            } catch (SQLException e ) {
+                System.out.println("Error en procesar la consulta "+ strQuery);
+            }
+        }
+        return result;
+    }
+
+    public static void finalizar() {
+    	if(BD.conexion != null ) {
+	        try {
+	            BD.conexion.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+    	}
+    }
+
+    public void finalize() {
+    	BD.finalizar();
+    }
 }
