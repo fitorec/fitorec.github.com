fdc83c4756c4276b2a79db49d2410d2df00aa6f4
Fitorec <chanerec@gmail.com>
2017-05-06 08:05:04 -0500

    Agregando contenidos en programacion oriantada a objetos

---
+++ itsmigra/programacion_orientada_objetos/codigos/alumno.py
@@ -0,0 +1,7 @@
+class Alumno:
+	def __privado(self):
+		print("entro a privado");
+
+alum = Alumno()
+alum.__privado()
+# alum._Alumno__privado()
--- proyectos/fCalendar/src/fcal-lang-en.js
+++
@@ -1,8 +0,0 @@
-fcal_days = ['mon','tue','wed','thu','fri','sat','sun']
-fcal_months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-
-fcal_next_month_label = 'View the next Month'
-fcal_prev_month_label = 'View the preview Month'
-
-
-
